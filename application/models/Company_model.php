db2b0486c22aa0167af36ffb086274f95372dbb5
Qsystem <devibong@qsystem-online.com>
2019-08-24 16:05:33 +0700

    devibong 20190824 16:05

---
+++ application/models/Company_model.php
@@ -0,0 +1,37 @@
+<?php 
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Company_model extends MY_Model {
+	public $tableName = "tbcompany";
+	public $pkey = "fst_code";
+	
+	public function  __construct(){
+		parent::__construct();
+	}
+
+	
+
+	public function getRules($mode="ADD",$id=0){
+		$rules = [];
+		return $rules;		
+    }
+    
+    public function getData(){
+        $ssql = "select * from ". $this->tableName;
+        $query = $this->db->query($ssql,[]);
+        $result = $query->result();
+        return $result;
+    }
+
+    public function getDataByAppId($appId){
+
+        $ssql = "select b.fst_code,b.fst_name from tbappid a inner join tbcompany b on a.fst_company_code = b.fst_code
+            where a.fst_appid = ?";
+
+        $query = $this->db->query($ssql,[$appId]);
+        $result = $query->result();
+        return $result;
+    }
+	
+
+}
