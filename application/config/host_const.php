07998bcd72734b87c660b6abad580e1556f70c3f
devibong <devibong@qsystem-online.com>
2021-07-29 16:51:59 +0700

    devibong

--- application/config/host_const.php
+++ application/config/host_const.php
@@ -1,3 +1,4 @@
 <?php
-
-defined('BASE_URL') OR define ('BASE_URL','http://armex2.qsystem-online.com');
+defined('BASE_URL') OR define ('BASE_URL','http://'. $_SERVER["HTTP_HOST"] .'/greebel_mobile');
+//defined('COMPONENT_URL') OR define ('COMPONENT_URL','http://localhost/component');
+defined('COMPONENT_URL') OR define ('COMPONENT_URL','http://greebel.qsystem-online.com/');
